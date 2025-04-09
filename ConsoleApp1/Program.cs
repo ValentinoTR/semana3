b82e6c2bfe85686121483bbb2fd50c4421f5bd3e
Marcelo <lolofede168@gmail.com>
2025-04-09 10:03:40 -0500

    primer commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Bienvenido al curso de c#");
+            Console.ReadKey();
+        }
+    }
+}
